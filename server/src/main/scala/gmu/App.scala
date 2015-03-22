939be7e7ee8084e74f86c5a7f2d0ec3dea51155f
Bryan Absher <bryan.absher@gmail.com>
2015-03-22 15:38:48 -0400

    removed test actor

--- server/src/main/scala/gmu/App.scala
+++ server/src/main/scala/gmu/App.scala
@@ -8,16 +8,6 @@ object HelloRemote extends App  {
   val config = ConfigFactory.load()
   val system = ActorSystem("Zerg", config)
   val router = system.actorOf(FromConfig.props(Props[ZergWorker]), "zergRouter")
-  val remoteActor = system.actorOf(Props[RemoteActor], name = "RemoteActor")
-  remoteActor ! "The RemoteActor is alive"
-}
-
-class RemoteActor extends Actor {
-  def receive = {
-    case msg: String =>
-      println(s"RemoteActor received message '$msg'")
-      sender ! "Hello from the RemoteActor"
-  }
 }
 
 class ZergWorker extends Actor {
