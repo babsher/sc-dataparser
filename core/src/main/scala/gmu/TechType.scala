3db8acf03a9f60acb09f4fbbf256073fa4812224
Bryan Absher <bryan.absher@gmail.com>
2015-03-26 02:18:48 -0400

    added upgrades and tech

---
+++ core/src/main/scala/gmu/TechType.scala
@@ -0,0 +1,45 @@
+package gmu
+
+/**
+ * Created by bryan on 3/26/15.
+ */
+class TechType extends Enumeration {
+  type TechType = Value
+  val
+  Stim_Packs,
+  Lockdown,
+  EMP_Shockwave,
+  Spider_Mines,
+  Scanner_Sweep,
+  Tank_Siege_Mode,
+  Defensive_Matrix,
+  Irradiate,
+  Yamato_Gun,
+  Cloaking_Field,
+  Personnel_Cloaking,
+  Burrowing,
+  Infestation,
+  Spawn_Broodlings,
+  Dark_Swarm,
+  Plague,
+  Consume,
+  Ensnare,
+  Parasite,
+  Psionic_Storm,
+  Hallucination,
+  Recall,
+  Stasis_Field,
+  Archon_Warp,
+  Restoration,
+  Disruption_Web,
+  Mind_Control,
+  Dark_Archon_Meld,
+  Feedback,
+  Optical_Flare,
+  Maelstrom,
+  Lurker_Aspect,
+  Healing,
+  None,
+  Unknown,
+  Nuclear_Strike = Value
+}
